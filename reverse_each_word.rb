c05a43e6ed4117f48b3d9ee2c6ea9d8178dfe397
Liselle Logan <liselle.logan@gmail.com>
2020-09-17 03:59:26 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,3 +6,11 @@ def reverse_each_word(string)
   end
   reversed_array.join(" ")
 end
+
+def reverse_each_word(string)
+  array = string.split(" ")
+  array.collect do |word|
+    reversed_array << word.reverse
+  end
+  array
+end
